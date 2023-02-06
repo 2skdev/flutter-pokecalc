189f5c95637eb91fe03f53fa13cf0094414038e9
戸崎巧 <tosaki@tozakitakuminoMacBook-Air.local>
2023-02-07 02:13:25 +0900

    :sparkles: add util methods to Stats class

--- lib/models/stats.dart
+++ lib/models/stats.dart
@@ -2,33 +2,50 @@ import 'package:flutter/material.dart';
 
 @immutable
 class Stats {
-  final int h;
-  final int a;
-  final int b;
-  final int c;
-  final int d;
-  final int s;
-
+  /// ステータスクラスを生成する
   const Stats({
-    required this.h,
-    required this.a,
-    required this.b,
-    required this.c,
-    required this.d,
-    required this.s,
+    this.h = 0,
+    this.a = 0,
+    this.b = 0,
+    this.c = 0,
+    this.d = 0,
+    this.s = 0,
   });
 
-  static Stats get zeros => const Stats(h: 0, a: 0, b: 0, c: 0, d: 0, s: 0);
+  /// HP
+  final int h;
 
-  static List<String> get strings => [
-        'HP',
-        '攻撃',
-        '防御',
-        '特攻',
-        '特防',
-        '素早',
-      ];
+  /// 攻撃
+  final int a;
 
+  /// 防御
+  final int b;
+
+  /// 特攻
+  final int c;
+
+  /// 特防
+  final int d;
+
+  /// 素早
+  final int s;
+
+  /// ステータスの文字列のリスト
+  static List<String> get strings => ['HP', '攻撃', '防御', '特攻', '特防', '素早'];
+
+  /// ステータスのアルファベットのリスト
+  static List<String> get initials => ['H', 'A', 'B', 'C', 'D', 'S'];
+
+  /// ステータスをindexと値でループする
+  void forEach(Function(int index, int value) action) {
+    for (var i = 0; i < 6; i++) {
+      action(i, this[i]);
+    }
+  }
+
+  /// 配列からStatsクラスを生成する
+  ///
+  /// [array]のサイズがStatsの数(6)と一致しない時はアサート
   static Stats fromArray(List<int> array) {
     assert(array.length == 6);
 
@@ -42,32 +59,34 @@ class Stats {
     );
   }
 
+  /// Statsクラスから配列を生成する
   List<int> toArray() {
     return [h, a, b, c, d, s];
   }
 
+  /// 配列参照
+  ///
+  /// 一度配列に変換して参照する
+  /// [index]がStatsの数(6)未満でない時はアサート
   int operator [](int index) {
     assert(index < 6);
     return toArray()[index];
   }
 
+  /// 文字列変換
   @override
   String toString() {
     return '$h-$a-$b-$c-$d-$s';
   }
 
-  Stats copyWithIndex(int index, value) {
+  /// 指定のindexの値を置き換えて複製する
+  Stats copyWithIndex(int index, int value) {
+    assert(index < 6);
     return fromArray(toArray()..[index] = value);
   }
 
-  Stats copyWith({
-    int? h,
-    int? a,
-    int? b,
-    int? c,
-    int? d,
-    int? s,
-  }) {
+  /// null以外の値を置き換えて複製する
+  Stats copyWith({int? h, int? a, int? b, int? c, int? d, int? s}) {
     return Stats(
       h: h ?? this.h,
       a: a ?? this.a,
